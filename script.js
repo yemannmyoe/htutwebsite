56f55c88b40efafb8b6fc45f50546cca58fbb86f
yemann myoe <yemannmyoe1876@gmail.com>
2024-01-02 23:25:18 +0630

    responsive

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ controls.addEventListener("mouseout", () => {
 const slideshowDivs = () => {
   for (let i = 1; i <= 5; i++) {
     const div = document.createElement("div");
-    div.style.backgroundImage = `url(images/slideshow/section-1-bg-${i}.jpg)`;
+    div.style.backgroundImage = `url(images/slideshow/section-1-bg-${i}.png)`;
 
     i === 1 && div.classList.add("active");
 
